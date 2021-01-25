8cab06b65d514c769e887387f059429058ee1663
reybos <andreybosiy@yandex.ru>
2021-01-25 16:15:45 +0300

    3. Прерывание нити [#456172]

--- src/main/java/ru/job4j/concurrent/ConsoleProgress.java
+++ src/main/java/ru/job4j/concurrent/ConsoleProgress.java
@@ -7,7 +7,10 @@ public class ConsoleProgress implements Runnable {
         char[] process = new char[] {'/', '|', '\\'};
         try {
             while (!Thread.currentThread().isInterrupted()) {
-                System.out.print("\rLoading ... " + process[count++ % 3]);
+                System.out.print("\rLoading ... " + process[count++]);
+                if (count == process.length) {
+                    count = 0;
+                }
                 Thread.sleep(200);
             }
         } catch (InterruptedException e) {
