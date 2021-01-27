70801667bf3cb46ad486091c3f2565f25caf18ca
reybos <andreybosiy@yandex.ru>
2021-01-27 22:14:48 +0300

    1. Visibility. Общий ресурс вне критической секции [#456182]

--- src/main/java/ru/job4j/resource/synchronization/ParseFile.java
+++ src/main/java/ru/job4j/resource/synchronization/ParseFile.java
@@ -22,16 +22,16 @@ public class ParseFile {
     }
 
     public synchronized String getContentWithoutUnicode() throws IOException {
-        String output = "";
-        try (InputStream i = new FileInputStream(file)) {
+        StringBuilder output = new StringBuilder();
+        try (BufferedReader in = new BufferedReader(new FileReader(file))) {
             int data;
-            while ((data = i.read()) > 0) {
+            while ((data = in.read()) > 0) {
                 if (data < 0x80) {
-                    output += (char) data;
+                    output.append((char) data);
                 }
             }
         }
-        return output;
+        return output.toString();
     }
 
     public synchronized void saveContent(String content) throws IOException {
