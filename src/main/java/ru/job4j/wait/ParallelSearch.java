fc59ecc72faf59f53aac343d67df38b1e8716989
reybos <andreybosiy@yandex.ru>
2021-01-29 23:22:23 +0300

    2. Обеспечить остановку потребителя. [#456162]

---
+++ src/main/java/ru/job4j/wait/ParallelSearch.java
@@ -0,0 +1,32 @@
+package ru.job4j.wait;
+
+public class ParallelSearch {
+    public static void main(String[] args) {
+        SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>();
+        final Thread consumer = new Thread(
+                () -> {
+                    while (!Thread.currentThread().isInterrupted()) {
+                        try {
+                            System.out.println(queue.poll());
+                        } catch (InterruptedException e) {
+                            Thread.currentThread().interrupt();
+                        }
+                    }
+                }
+        );
+        consumer.start();
+        new Thread(
+                () -> {
+                    for (int index = 0; index != 3; index++) {
+                        queue.offer(index);
+                        try {
+                            Thread.sleep(500);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    consumer.interrupt();
+                }
+        ).start();
+    }
+}
